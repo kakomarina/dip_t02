3386c309d598764398a0b273cbfc8fad47a5158e
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2020-05-01 16:18:45 -0300

    laplacian filter added

---
+++ laplacian_filter.py
@@ -0,0 +1,57 @@
+import numpy as np
+import imageio
+import math
+
+
+def image_convolution(img, convolution_filter):
+    n, m = convolution_filter.shape
+    N, M = img.shape
+
+    a = int((n-1)/2)
+    b = int((m-1)/2)
+
+    # creating a zero padded image
+    img_padded = np.pad(img, int(n/2))
+
+    # image after filter
+    image_filtered = np.zeros(img.shape, dtype=np.uint8)
+
+    # for all pixels the original image
+    for x in range(a, N+a):
+        for y in range(b, M+b):
+            # region centered at x,y
+            region_to_be_filtered = img_padded[x-a:x+a+1, y-b:y+b+1]
+
+            # convoluting filter and image region
+            image_filtered[x-a][y-b] = np.sum(np.multiply(
+                region_to_be_filtered, convolution_filter)).astype(np.uint8)
+
+    return image_filtered
+
+
+# Normalizing a image
+# max_scale_val is equal to the maximum value in a certain scale
+# 8 bit greyscale maximum is 255
+def image_normalization(img, max_scale_val):
+    normilzed_image = ((img - np.min(img)) * max_scale_val) / np.max(img)
+    return normilzed_image
+
+
+# Adds two images values, multiplying the filtered one by c
+def filter_addition(img, filtered, c):
+    image_added = img + filtered * c
+    return image_added
+
+
+def laplacian_filter(img, c, kernel):
+    # Image padded convolution with given kernel
+    convoluted_image = image_convolution(img, kernel)
+
+    # Imag scaling with normalization function
+    normalized_image = image_normalization(convoluted_image, 255)
+
+    # Adding filtered image * c with original image
+    image_added = filter_addition(img, normalized_image, c)
+
+    # Scaling final image and returning it
+    return image_normalization(image_added, 255)
--- main.py
+++ main.py
@@ -2,65 +2,12 @@ import numpy as np
 import imageio
 import math
 
+from bilateral_filter import *
+from laplacian_filter import *
+
 
 def RSE(input_img, output_img):
     return np.sqrt(np.sum(np.power(output_img.astype(np.float32) - input_img.astype(np.float32), 2)))
-
-
-def gaussianKernelEquation(x, sigma):
-    return (1.0 / (2.0 * np.pi * (np.power(sigma, 2)))) * (np.exp(-(np.power(x, 2)) / (2.0 * (np.power(sigma, 2)))))
-
-
-def createSpatialGaussianComponent(sigmaS, n):
-    spatial_gaussian = np.zeros((n, n), dtype=np.float32)
-
-    for x in range(0, n):
-        for y in range(0, n):
-            spatial_gaussian[x][y] = gaussianKernelEquation(
-                euclidian_distance(int((n-1)/2), int((n-1) / 2), x, y), sigmaS
-            )
-
-    return spatial_gaussian
-
-
-def euclidian_distance(cx, cy, x, y):
-    return np.sqrt((x - cx) ** 2 + (y - cy) ** 2)
-
-
-def bilateral_filter(f, spatial_gaussian, sigmaR):
-    N, M = f.shape
-    n, m = spatial_gaussian.shape
-
-    a = int((n - 1) / 2)
-    b = int((m - 1) / 2)
-
-    # new image to store filtered pixels
-    g = np.zeros(f.shape, dtype=np.uint8)
-
-    # calculating padded image
-    padded = np.pad(f, max(a, b))
-
-    # for every pixel
-    for x in range(a, N+a):
-        for y in range(b, M+b):
-            # passo 1: calcular a range Gaussian
-            range_gaussian = np.zeros((n, m), dtype=np.float32)
-            Wp = 0
-            If = 0.0
-            # para cada vizinho
-            for xi in range(x - a, x + a + 1):
-                for yi in range(y - b, y + b + 1):
-                    # calcular a range gaussian
-                    range_gaussian[xi - (x - a)][yi - (y - b)] = gaussianKernelEquation(
-                        (padded[xi][yi]*1.0) - (padded[x][y]*1.0), sigmaR)
-                    # calcular wi para o pixel correspondente
-                    wi = range_gaussian[xi - (x - a)][yi -
-                                                      (y - b)] * spatial_gaussian[xi - (x - a)][yi - (y - b)]
-                    Wp = float(Wp + wi)
-                    If = If + float(wi * padded[xi][yi])
-            g[x-a][y-b] = int(If / Wp)
-
-    return g
 
 
 filename = str(input()).rstrip()
@@ -78,7 +25,17 @@ if method == 1:
 
 elif method == 2:
     c = float(input())
-    kernel = int(input())
+    kernel_type = int(input())
+
+    # creating kernel1
+    if kernel_type == 1:
+        kernel = np.matrix([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+
+    # creating kernel2
+    elif kernel_type == 2:
+        kernel = np.matrix([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+
+    output_image = laplacian_filter(input_img, c, kernel)
 
 elif method == 3:
     sigmaRow = float(input())
